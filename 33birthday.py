35772a5da440758410c124048a8faf76bc692bc9
00chunyan00 <chunyanlin@Chunyans-MBP.attlocal.net>
2023-02-19 20:25:51 -0800

    update

--- 33birthday.py
+++ 33birthday.py
@@ -12,17 +12,29 @@
 import sys
 import random
 
-
 days = int(sys.argv[1])
 people = int(sys.argv[2])
-run = 10
+run = 100000
 shared = 0
-for i in sys.argv[1:]:
-	for bd in range(run):
-#I am not done yet....
-				
-print(shared/run)
-"""
+
+#set number of trails to run
+for trails in range(run):
+	found = False
+	cal = [0 for i in range(days)]  #assign 0 to all the calendar dates
+
+	for personel in range(people):
+		birthday = random.randint(0, days - 1) #generate random birthday
+		cal[birthday] += 1
+		
+	for val in cal:
+		if val > 1:
+			found = True
+			break
+	if found:
+		shared += 1
+print(f'{shared/run:.3f}')
+
+"""	
 python3 33birthday.py 365 23
 0.571
 """
